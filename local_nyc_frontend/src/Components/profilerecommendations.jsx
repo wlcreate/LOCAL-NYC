79e1376e773f4dfb6d3fd78f4d26e08f9af1b7de
wlcreate <waverley.nyc@gmail.com>
2020-10-19 19:43:19 -0400

    fixes keep_logged_in

--- local_nyc_frontend/src/Components/profilerecommendations.jsx
+++ local_nyc_frontend/src/Components/profilerecommendations.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class ProfileRecommendations extends React.Component{
     render(){
-        debugger
+        // debugger
         return(
         <h1>Name: {this.props.user.username}</h1>
         )
