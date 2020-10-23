384af795d7892fabbed37e8e44fa01809b11a5f7
wlcreate <waverley.nyc@gmail.com>
2020-10-23 09:32:25 -0400

    DONE

--- local_nyc_frontend/src/Components/Home/HomeCarousel.jsx
+++ local_nyc_frontend/src/Components/Home/HomeCarousel.jsx
@@ -4,18 +4,18 @@ import { Carousel } from 'react-responsive-carousel';
 class HomeCarousel extends React.Component {
    render() {
         return (
-         <Carousel>
+         <Carousel id="home-carousel">
          <div>
              <img src="https://images.unsplash.com/photo-1496442226666-8d4d0e62e6e9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt="image 1" />
-             <p className="legend">Legend 1</p>
+             {/* <p className="legend">Legend 1</p> */}
         </div>
         <div>
              <img src="https://images.unsplash.com/photo-1587162147120-430be9b33be3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt="image 2"/>
-             <p className="legend">Legend 2</p>
+             {/* <p className="legend">Legend 2</p> */}
          </div>
          <div>
              <img src="https://images.unsplash.com/photo-1572984128466-7190c176569b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60" alt="image 3"/>
-             <p className="legend">Legend 3</p>
+             {/* <p className="legend">Legend 3</p> */}
          </div>
         </Carousel>
       );
--- local_nyc_frontend/src/Components/Home/home.jsx
+++ local_nyc_frontend/src/Components/Home/home.jsx
@@ -10,6 +10,30 @@ class Home extends React.Component {
                 <Header />
                 {/* <img id="home-image" src={require('./Home_Image.png')}  alt="Home Image"/> */}
                 <HomeCarousel />
+                <div id="about-us">
+                    <h2>About Us</h2>
+                    <h3>Wanna know where the locals go?</h3>
+                    <p>So do we! The creators of this app currently live in NYC and are always ready to explore their home. But as many know, NYC is a HUGE city that is constantly changing. Like other locals, we don't want to go to the tourist spots; in fact we actively avoid those areas as much as possible (looking at you Times Square). That being said, in a city so big, how do we find new spots? How do we learn about other local spots to give our business to?</p>
+                    <p>No need to fear, just like Spiderman, The LOCAL NYC is here to fight for us little guys!</p>
+                    <p>To use the LOCAL NYC:</p>
+                    <ol>
+                        <li>Log in (or register if you don't have an account)</li>
+                        <li>After you log in you will be brought to your profile page where you can edit your profile and see all of your recommendations</li>
+                        <li>To start checking out other spots, click "Neighborhoods" on the navigation bar where you can see different neighborhoods in NYC separated by borough</li>
+                        <li>Once you click on a neighborhood you can see all the spots other locals have added</li>
+                            <ul>
+                                <li>Search for a specific spot by name by typing into the search bar</li>
+                                <li>Filter spots by a specific category</li>
+                                <li>Don't see a spot listed? Click "Add a Spot"!</li>
+                            </ul>
+                        <li>Once you click on a Spot you can see information about a Spot and what locals recommend at the Spot</li>
+                            <ul>
+                                <li>You can "Add a Recommendation" and fill out what you recommend what you like about this spot</li>
+                                <li>You can also delete your recommendation if you made a mistake!</li>
+                            </ul>
+                        <li>Once you are finished you can log out!</li>
+                    </ol>
+                </div>
             </div>
             
         )
